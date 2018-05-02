7dcd25c8468cb492ab9bf60744f75f049b3d2fdc
Ankur Patro <ankur.patro@nagarro.com>
2018-05-02 15:44:57 +0530

    Automation Script modifications

--- test/ride.bat
+++ test/ride.bat
@@ -7,9 +7,4 @@ set CLASSPATH="%~dp0\lib\itpa-keywords-1.1.0.jar;%CLASSPATH%"
 
 echo %CLASSPATH%
 
-<<<<<<< HEAD
-ride.py .\Test_OpenShift
-
-=======
 ride.py .\PCK.AUTOMIC_OPENSHIFT
->>>>>>> develop
